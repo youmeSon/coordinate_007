54681d1aa50be535855fa7548aefcd4b90b94224
Youme <winellyoume@gmail.com>
2021-03-09 11:19:05 +0000

    JS: Change code to improve performance

--- main.js
+++ main.js
@@ -5,15 +5,19 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', e => {
-  const x = e.clientX;
-  const y = e.clientY;
-  
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.innerHTML = `${x}px, ${y}px`;
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const halfTargetWidth = targetRect.width / 2;
+  const halfTargetHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', e => {
+    const x = e.clientX;
+    const y = e.clientY;
+    
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - halfTargetWidth}px,${y - halfTargetHeight}px)`;
+    tag.style.transform = `translate(${x}px,${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  })
 })
